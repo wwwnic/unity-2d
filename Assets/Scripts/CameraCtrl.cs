ad896810f39dc8e284895735c39f37051bda883c
SMartinCyr <smartincyr@dept-info.crosemont.quebec>
2021-02-24 22:20:01 -0500

    Correction script camera

--- Assets/Scripts/CameraCtrl.cs
+++ Assets/Scripts/CameraCtrl.cs
@@ -1,32 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Script de contrôle de la caméra.
-/// </summary>
 public class CameraCtrl : MonoBehaviour
 {
     private Rigidbody2D rb;
     private PersoCtrl personnage;
-    [SerializeField] float vitesse = 1.0f;
-    // La layer avec laquelle la caméra peut avoir une collision.
+    [SerializeField] float vitesseDeLaCameraEnX = 4.0f;
+    [SerializeField] float vitesseDeLaCameraEnY = 10.0f;
+    [SerializeField] float hauteurDeLaCamera = 2.5f;
     [SerializeField] LayerMask layerMask;
+
+
+    [SerializeField] float zoomFactor = 3f;
+    [SerializeField] float zoomLerpSpeed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         personnage = GameObject.FindWithTag("Player").GetComponent<PersoCtrl>();
         rb = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 posPerso = personnage.transform.position;
+        Vector3 posPersonage = personnage.transform.position;
         Vector3 posCamera = transform.position;
-        float distance = posCamera.x - posPerso.x;
+        float distanceEntrePersonnageEtCameraEnX = posPersonage.x - posCamera.x;
+        float distanceEntrePersonnageEtCameraEnY = posPersonage.y - posCamera.y + hauteurDeLaCamera;
 
-        rb.velocity = new Vector2(-distance * vitesse, 0);
+        rb.velocity = new Vector3(distanceEntrePersonnageEtCameraEnX * vitesseDeLaCameraEnX, distanceEntrePersonnageEtCameraEnY * vitesseDeLaCameraEnY);
     }
+
 }
