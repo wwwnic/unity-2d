053f40829b6a0e7356ed6b8404ff3c54ea66c608
Nicolas <nbrunet@dept-info.crosemont.quebec>
2021-03-02 22:37:11 -0500

    Correction salle 4, ajout salle 5 et nettoyage

--- Assets/Scripts/SalleDeJeu/Salle4/SalleCtrl04.cs
+++ Assets/Scripts/SalleDeJeu/Salle4/SalleCtrl04.cs
@@ -6,24 +6,27 @@ namespace SalleDeJeu
 {
     public class SalleCtrl04 : LogiqueDesSalles
     {
-        bool _resultatColonneOR1_2;
-        bool _resultatColonneNAND3_4;
-        bool _resultatColonneNOT5;
-        bool _resultatColonneXNOR1_2;
-        bool _resultatColonneXNOR3_4;
-        bool _resultatFinalColonneAND;
+        bool _resultatColonneOR1_1;
+        bool _resultatColonneNAND1_2;
+        bool _resultatColonneNO1_3;
+
+        bool _resultatColonneXNOR2_1;
+        bool _resultatColonneXNOR2_2;
+
+        bool _resultatFinalColonneAND3_1;
 
         public override void DetectionChangementObjetActionnable()
         {
-            _resultatColonneOR1_2 = ComparateurBooleen(BooleanOperation.ou_OR, objectActionnableList[0], objectActionnableList[1]);
-            _resultatColonneNAND3_4 = ComparateurBooleen(BooleanOperation.nonEt_NAND, objectActionnableList[2], objectActionnableList[3]);
-            _resultatColonneNOT5 = ComparateurBooleen(BooleanOperation.non_NO, objectActionnableList[4], objectActionnableList[4]);
-            _resultatColonneXNOR1_2 = ComparateurBooleen(BooleanOperation.nonOuExclusif_XNOR, _resultatColonneOR1_2, _resultatColonneNAND3_4);
-            _resultatColonneXNOR3_4 = ComparateurBooleen(BooleanOperation.nonOuExclusif_XNOR, _resultatColonneNOT5, objectActionnableList[5]);
+            _resultatColonneOR1_1 = ComparateurBooleen(BooleanOperation.ou_OR, objectActionnableList[0], objectActionnableList[1]);
+            _resultatColonneNAND1_2 = ComparateurBooleen(BooleanOperation.nonEt_NAND, objectActionnableList[2], objectActionnableList[3]);
+            _resultatColonneNO1_3 = !objectActionnableList[4].Get_isActivated();
 
-            _resultatFinalColonneAND = ComparateurBooleen(BooleanOperation.et_AND, _resultatColonneXNOR1_2, _resultatColonneXNOR3_4);
+            _resultatColonneXNOR2_1 = ComparateurBooleen(BooleanOperation.nonOuExclusif_XNOR, _resultatColonneOR1_1, _resultatColonneNAND1_2);
+            _resultatColonneXNOR2_2 = ComparateurBooleen(BooleanOperation.nonOuExclusif_XNOR, _resultatColonneNO1_3, objectActionnableList[5].Get_isActivated());
 
-            objectAnimatorSetParameterBool(_resultatFinalColonneAND);
+
+            _resultatFinalColonneAND3_1 = ComparateurBooleen(BooleanOperation.et_AND, _resultatColonneXNOR2_1, _resultatColonneXNOR2_2);
+            objectAnimatorSetParameterBool(_resultatFinalColonneAND3_1);
 
         }
     }
